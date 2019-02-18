90dc6cbdedcd1a0e2fd2eedb8cbebccfaf8f3695
mantasms <m.sakaitis@gmail.com>
2019-02-18 11:35:27 +0200

    added new field 'label' to array

--- create.php
+++ create.php
@@ -5,7 +5,7 @@ require_once 'functions/form.php';
 require_once 'functions/file.php';
 
 function form_success($safe_input, $form) {
-	// TO DO
+    // TO DO
 }
 
 function form_fail($safe_input, $form) {
@@ -15,7 +15,7 @@ function form_fail($safe_input, $form) {
 $form = [
     'fields' => [
         'team_name' => [
-            'label' => '',
+            'label' => 'Create team',
             'type' => 'text',
             'placeholder' => 'Team name',
             'validate' => [
@@ -56,21 +56,24 @@ if (!empty($_POST)) {
     </head>
     <body>
         <h1>Call Friday</h1>
-		<?php if ($show_form): ?>
-			<form method="POST">
-				<?php foreach ($form['fields'] as $field_id => $field): ?>
-					<label>
-						<input type="<?php print $field['type']; ?>" 
-							   name="<?php print $field_id; ?>" 
-							   placeholder="<?php print $field['placeholder']; ?>"/>
-					</label>
-				<?php endforeach; ?>
-				<?php foreach ($form['buttons'] as $button_id => $button): ?>
-					<button name="action" value="<?php print $button_id; ?>">
-						<?php print $button['text']; ?>
-					</button>
-				<?php endforeach; ?>
-			</form>
-		<?php endif; ?>
+        <form method="POST">
+            <?php if ($show_form): ?>
+                <form method="POST">
+                    <?php foreach ($form['fields'] as $field_id => $field): ?>
+                        <span><?php print $field['label']; ?></span>
+                        <label>
+                            <input type="<?php print $field['type']; ?>" 
+                                   name="<?php print $field_id; ?>" 
+                                   placeholder="<?php print $field['placeholder']; ?>"/>
+                        </label>
+                    <?php endforeach; ?>
+                    <?php foreach ($form['buttons'] as $button_id => $button): ?>
+                        <button name="action" value="<?php print $button_id; ?>">
+                            <?php print $button['text']; ?>
+                        </button>
+                    <?php endforeach; ?>
+                </form>
+            <?php endif; ?>
+        </form>
     </body>
 </html>
